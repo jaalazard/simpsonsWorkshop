815ba92fcbf0ea39b696a7ea2bb29308457ff35b
jaalazard <ja.alazard@proton.me>
2023-09-21 12:30:04 +0200

    multi donut counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,52 @@
-import './App.css'
-import Avatar from './components/Avatar'
+import "./App.css";
+import Avatar from "./components/Avatar";
 
-const bart = {
-    image: "https://www.123-stickers.com/2104/bart-simpsons-fesses.jpg",
-    firstname: "Bart",
-    lastname: "Simpson",
-  };
-
-const homer = {
-    image: "https://e7.pngegg.com/pngimages/580/272/png-clipart-homer-simpson-illustration-homer-simpson-bart-simpson-s-guide-to-life-marge-simpson-bart-simpson.png",
-    firstname: "Homer",
-    lastname: "Simpson",
-  };
+const simpsons = [
+  {
+    name: "Homer Simpson",
+    image:
+      "https://www.pngarts.com/files/11/Homer-Simpson-Cartoon-PNG-Image-Background.png",
+    donut: 0,
+  },
+  {
+    name: "Marge Simpson",
+    image: "https://www.lessimpson.fr/wp-content/uploads/marge-simpson.webp",
+    donut: 0,
+  },
+  {
+    name: "Bart Simpson",
+    image: "https://img.favpng.com/15/0/9/bart-simpson-gif-image-desktop-wallpaper-cannabis-png-favpng-eHUMEtrV8nyYLwKeavNrEc3bb.jpg",
+    donut: 0,
+  },
+  {
+    name: "Lisa Simpson",
+    image:
+      "https://i.pinimg.com/originals/4d/63/fb/4d63fbc3629b6c145965922fb575d31e.png",
+    donut: 0,
+  },
+  {
+    name: "Maggie Simpson",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-D4-DO4xVHA-sqrHycG0KRuQFidrRrdcdCA&usqp=CAU",
+    donut: 0,
+  },
+  {
+    name: "Abraham Simpson",
+    image:
+      "https://www.simpsonspark.com/images/persos/contributions/abraham-simpson-22960.jpg",
+    donut: 0,
+  },
+];
 
 function App() {
-
   return (
     <>
-      <Avatar {...homer}  />
-      <Avatar {...bart} />
+      <h1>The Simspons</h1>
+      <div className="family">
+        <Avatar simpsons={simpsons} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,12 +1,21 @@
-const Avatar = ({image, firstname, lastname}) => (
-  <div>
+import React, { useState } from 'react';
+
+const Avatar = ({simpsons}) => {
+  const [donut, setDonut] = useState(0);
+
+  return (
+    simpsons.map((simpson) => (
+  <div key="simpson.name">
     <figure>
-      <img src={image} alt="un simpson"></img>
+      <img src={simpson.image} alt="simpson.name"></img>
       <figcaption>
-        {firstname} - {lastname}
+        {simpson.name} : {simpson.donut}
       </figcaption>
+      <button onClick={() => setDonut(simpson.donut++)}>🍩</button>
     </figure>
   </div>
+    ))
 );
+  }
 
 export default Avatar;
